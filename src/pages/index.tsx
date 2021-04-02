c6ad042b8873619166b15c0dc44cf28a5ee37188
MarcosRene <renemoura789@gmail.com>
2021-04-02 12:15:24 -0300

    :sparkles: feat: create Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,26 @@
 import { MetaTags } from '../components';
+import { Container, Section } from '../../styles/home';
 
 export default function Home() {
   return (
     <>
       <MetaTags title="Home" />
-      <h1>WELCOME</h1>
+
+      <Container>
+        <Section>
+          <span>👋️ Hey, welcome</span>
+
+          <h1>
+            News about <br />
+            the <strong>React</strong> world
+          </h1>
+          <p>
+            Get access to all the articles <strong>for U$9.90/month</strong>
+          </p>
+        </Section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </Container>
     </>
   );
 }
---
+++ styles/home.ts
@@ -0,0 +1,44 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.main`
+  ${({ theme }) => css`
+    width: 100%;
+    max-width: 63.75rem;
+    height: calc(100vh - 5rem);
+    margin: 0 auto;
+    padding: 0 2rem;
+    color: ${theme.colors.text};
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 1rem;
+  `}
+`;
+
+export const Section = styled.section`
+  ${({ theme }) => css`
+    font-size: 1.5rem;
+    font-weight: 700;
+
+    > h1 {
+      margin: 2.5rem 0 1.25rem;
+      font-size: 4.5rem;
+      font-weight: 900;
+      line-height: 4.5rem;
+
+      > strong {
+        color: ${theme.colors.blue};
+      }
+    }
+
+    > p {
+      font-weight: 400;
+
+      > strong {
+        color: ${theme.colors.blue};
+        display: block;
+      }
+    }
+  `}
+`;
