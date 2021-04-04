1faa4c98cbd853a04ec1933452a6c000edbc8fb7
MarcosRene <renemoura789@gmail.com>
2021-04-04 10:58:10 -0300

    :recycle: chore: listen to more webhook events

--- src/pages/api/lib/manage-subscription/index.ts
+++ src/pages/api/lib/manage-subscription/index.ts
@@ -6,16 +6,17 @@ import { getUserByStripeId } from '../../utils';
 type SaveSubscription = {
   subscriptionId: string;
   customerId: string;
+  isCreateSubscription?: boolean;
 };
 
 type UserRef = {
   id: string;
 };
 
-export const saveSubscription = async ({
-  subscriptionId,
-  customerId,
-}: SaveSubscription) => {
+export const saveSubscription = async (
+  { subscriptionId, customerId }: SaveSubscription,
+  isCreateSubscription = false,
+) => {
   const userRefPromise = await fauna.query<UserRef>(
     q.Select('ref', q.Get(getUserByStripeId(customerId))),
   );
@@ -34,9 +35,21 @@ export const saveSubscription = async ({
     price_id: subscription.items.data[0].price.id,
   };
 
-  await fauna.query(
-    q.Create(q.Collection('subscriptions'), {
-      data: subscriptionData,
-    }),
-  );
+  if (isCreateSubscription) {
+    await fauna.query(
+      q.Create(q.Collection('subscriptions'), {
+        data: subscriptionData,
+      }),
+    );
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          'ref',
+          q.Get(q.Match(q.Index('subscription_by_id'), subscriptionId)),
+        ),
+        { data: { subscriptionData } },
+      ),
+    );
+  }
 };
